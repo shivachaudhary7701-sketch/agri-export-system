e3c3d392b9fdc14929a96d615443507279a4bab4
Shiva Chaudhary <shivachaudhary7701@gmail.com>
2026-04-04 22:18:25 +0530

    fix not found error

--- server.js
+++ server.js
@@ -2,10 +2,13 @@
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static("public"));
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
 
 let exportsData = [];
 
